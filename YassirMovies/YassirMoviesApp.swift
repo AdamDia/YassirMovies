66e54d591e87e81e2f9f688d01cec712569bebb8
Adam EM <adam.diavolo@gmail.com>
2023-03-30 23:30:40 +0200

    Initial Commit

---
+++ YassirMovies/YassirMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirMoviesApp.swift
+//  YassirMovies
+//
+//  Created by Adam Essam on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct YassirMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
